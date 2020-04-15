527a3638ced79a9bee9ec5bf89fdb15a4f9263a3
Raffly <rafflypohan17@gmail.com>
2020-04-16 00:32:54 +0800

    Titipan & Data Kas

---
+++ Data_Kas.py
@@ -0,0 +1,42 @@
+import datetime
+
+class DataKas:
+    
+    # Constructor
+    def __init__(self, idKas, tanggal, pemasukan, pengeluaran, labaBersih):
+        self.__idKas = idKas
+        self.__tanggal = tanggal
+        self.__pemasukan = pemasukan
+        self.__pengeluaran = pengeluaran
+        self.__labaBersih = labaBersih
+
+    # Getter dan Setter
+    def getIdKas(self):
+        return self.__idKas
+    
+    def setIdKas(self, x):
+        self.__idKas = x
+    
+    def getTanggal(self):
+        return self.__tanggal
+    
+    def setTanggal(self, y, m, d):
+        self.__tanggal = datetime.date(y, m, d)
+
+    def getPemasukan(self):
+        return self.__pemasukan
+    
+    def setPemasukan(self, x):
+        self.__pemasukan = x
+
+    def getPengeluaran(self):
+        return self.__pengeluaran
+    
+    def setPengeluaran(self, x):
+        self.__pengeluaran = x
+    
+    def getLabaBersih(self):
+        return self.__labaBersih
+    
+    def setLabaBersih(self):
+        self.__labaBersih = self.__pemasukan - self.__pengeluaran
---
+++ Titipan.py
@@ -0,0 +1,51 @@
+import datetime
+
+class Titipan:
+    
+    # Constructor
+    def __init__(self, idBarang, namaPenitip, namaBarang, harga, jumlahTitipan, tanggal):
+        self.__idBarang = idBarang
+        self.__namaPenitip = namaPenitip
+        self.__namaBarang = namaBarang
+        self.__harga = harga
+        self.__jumlahTitipan = jumlahTitipan
+        self.__tanggal = tanggal
+    
+    # Getter dan Setter
+    def getIdBarang(self):
+        return self.__idBarang
+    
+    def setIdBarang(self, x):
+        self.__idBarang = x
+    
+    def getNamPenitip(self):
+        return self.__namaPenitip
+    
+    def setNamaPenitip(self, x):
+        self.__namaPenitip = x
+
+    def getNamaBarang(self):
+        return self.__namaBarang
+    
+    def setNamaBarang(self, x):
+        self.__namaBarang = x
+
+    def getHarga(self):
+        return self.__harga
+    
+    def setHarga(self, x):
+        self.__harga = x
+
+    def getJumlahTitipan(self):
+        return self.__jumlahTitipan
+    
+    def setJumlahTitipan(self, x):
+        self.__jumlahTitipan = x
+
+    def getTanggal(self):
+        return self.__tanggal
+    
+    def setTanggal(self, y, m, d):
+        self.__tanggal = datetime.date(y, m, d)    
+    
+    
